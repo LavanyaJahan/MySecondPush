a76b14f81acd3497cced249972e0e5272adf11d3
LavanyaJahan <lavanyamanickameee@gmail.com>
2022-10-13 15:02:57 +0530

    this projects is committed

---
+++ src/mydemopackage/MyFirstjavaApp.java
@@ -0,0 +1,14 @@
+package mydemopackage;
+
+import java.util.Date;
+
+public class MyFirstjavaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi ");
+		System.out.println(new Date());
+
+	}
+
+}
